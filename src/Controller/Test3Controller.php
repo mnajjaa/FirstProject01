d5b5d9b47648178bd459abcad2e8bffcefc6f64b
mnajjaa <mnajjaibtihel@gmail.com>
2023-09-25 11:19:50 +0100

    first commit

---
+++ src/Controller/Test3Controller.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class Test3Controller extends AbstractController
+{
+    #[Route('/test3', name: 'app_test3')]
+    public function index(): Response
+    {
+        return $this->render('test3/index.html.twig', [
+            'controller_name' => 'Test3Controller',
+            // //write i simple msg
+            'msg' => 'Hello World',
+        ]);
+    }
+
+    #[Route('/ttt/{name}', name: 'app_test3_name')]
+    public function msgesprit($name): Response
+    {
+        return $this->render('test3/msg.html.twig', [
+            'controller_name' => 'Test3Controller',
+            'n' => name,
+        ]);
+    }
+
+    #[Route('/details', name: 'app_test3')]
+    public function detailsAuthor(): Response
+    {
+        return $this->render('test3/msg.html.twig', [
+            
+        ]);
+    }
+
+}
