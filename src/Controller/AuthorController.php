b43743e8501195bdba9735aff0a052948023ad8c
mnajjaa <mnajjaibtihel@gmail.com>
2023-10-15 19:24:31 +0100

    atelier 3 ex2

--- src/Controller/AuthorController.php
+++ src/Controller/AuthorController.php
@@ -20,4 +20,17 @@ public function showAuthor ($name){
 return $this->render('author/show.html.twig', ['n'=>$name]);
 }
 
+#[Route('/showlist', name: 'app_showlist')]
+public function list()
+{$authors = array(
+    array('id' => 1, 'picture' => '/images/Victor-Hugo.jpg','username' => 'Victor Hugo', 'email' =>
+    'victor.hugo@gmail.com ', 'nb_books' => 100),
+    array('id' => 2, 'picture' => '/images/william-shakespeare.jpg','username' => ' William Shakespeare', 'email' =>
+    ' william.shakespeare@gmail.com', 'nb_books' => 200 ),
+    array('id' => 3, 'picture' => '/images/Taha_Hussein.jpg','username' => 'Taha Hussein', 'email' =>
+    'taha.hussein@gmail.com', 'nb_books' => 300),
+    );
+    return $this->render('author/list.html.twig',['authors'=>$authors]);
+}
+
 }
